dbe0c73469d227af6547da8c0b192285cc9645c6
Olga Gerber <gerberolya@gmail.com>
2022-11-29 12:19:19 +0100

    Added assert login_page

--- test_login.py
+++ test_login.py
@@ -11,4 +11,5 @@ def test_login(browser):
     page = MainPage(browser, link)
     page.open_page()
     page.login()
+    assert browser.current_url == 'https://www.saucedemo.com/inventory.html', '!!!You did not entered!!!'
 
